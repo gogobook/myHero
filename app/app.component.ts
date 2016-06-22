9aa2ca82f092a7b3cbb5be4f469e3a912140aa48
gogobook <O968041428@gmail.com>
2016-06-22 11:49:41 +0800

    主從架構練習框架

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,32 @@
 import {Component} from '@angular/core';
 
+export class Hero{
+    id:number;
+    name:string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>My Heroes</h2>
+     
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'Tour of Heroes';
+   
+}
+
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
